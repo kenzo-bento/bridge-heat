85a83dc544e64b13a7bdf43bf208e14f864d5640
kenzo-bento <notkenneth57@gmail.com>
2026-03-01 19:09:24 -0500

    Add BridgeHeat sensor with state attributes

--- custom_components/bridge_heat/sensor.py
+++ custom_components/bridge_heat/sensor.py
@@ -1,18 +1,29 @@
 from homeassistant.helpers.entity import Entity
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    async_add_entities([TemperatureSensor()])
+    async_add_entities([BridgeHeat()])
 
 
-class TemperatureSensor(Entity):
-    _attr_name = "Temperature"
-    _attr_unique_id = "temperature"
-    _attr_unit_of_measurement = "°C"
+class BridgeHeat(Entity):
+    _attr_name = "Bridge Heat"
+    _attr_unique_id = "bridge_heat"
 
     @property
     def state(self):
         state = self.hass.states.get("weather.forecast_home")
         if state is None:
+            return "No Data"
+
+        return "Collecting Data"  # replace with real temperature
+    
+    @property
+    def extra_state_attributes(self):
+        state = self.hass.states.get("weather.forecast_home")
+        if state is None:
             return None
 
-        return state.attributes.get("temperature")  # replace with real temperature
+        return {
+            "temperature": state.attributes.get("temperature"),
+            "humidity": state.attributes.get("humidity"),
+            "wind_speed": state.attributes.get("wind_speed")
+        }
