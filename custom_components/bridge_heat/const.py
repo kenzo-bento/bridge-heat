4bd7c5a20e7b905e2b5f2b58fa3e67c75e8f5cc1
kenzo-bento <notkenneth57@gmail.com>
2026-04-22 22:05:44 -0400

    Update URL and add new sensor attributes
    Updated the URL for data retrieval and added new attributes for various sensors including light, HVAC, energy, noise, and air quality.

--- custom_components/bridge_heat/const.py
+++ custom_components/bridge_heat/const.py
@@ -3,10 +3,28 @@ PLATFORMS = ["sensor"]
 
 UPLOAD_INTERVAL = 60 * 24 * 60 # seconds
 SAMPLE_INTERVAL = 60 * 24 * 60
-URL = "https://ucnsqqxukixwcraqqdxk.supabase.co/rest/v1/Temperature"
+URL = "https://ucnsqqxukixwcraqqdxk.supabase.co/rest/v1/bridgeheat"
 KEY = "sb_secret_4jlAjWFgrbVh44Y_nkOvYw_Wj5D_gfU"
 
 TEMP = "Temperature"
 HUMIDITY = "Humidity"
 PRESSURE = "Pressure"
+LIGHT = "Light"
+HVAC = "HVAC"
+ENERGY = "Energy"
+NOISE = "Noise"
+AQ = "Air Quality"
 PERMS_TITLE = "Permissions"
+TEMP_ATTR = {"name" : "sensor.%temperature%", "device_class" : 'temperature', "units_of_measurement" : ["°C", "°F", "K"]}
+PRESSURE_ATTR = {"name" : "sensor.%pressure%", "device_class" : 'pressure', "units_of_measurement" : ["psi", "Pa", "kPa", "hPa", "mmHg", "bar", "mbar"]}
+HUMIDITY_ATTR = {"name" : "sensor.%humidity%", "device_class" : 'humidity'}
+LIGHT_ATTR = {"name" : "light.%"}
+HVAC_ATTR = {"name" : "climate.%"}
+ENERGY_ATTR = {"name" : "sensor.%energy%", "device_class" : "energy", "units_of_measurement": ["Wh", "kWh", "MWh"]}
+NOISE_ATTR = {"name" : "sensor.%noise%", "device_class" : "sound_pressure", "units_of_measurement" : ["dB", "dBA"]}
+AQ_ATTR = {
+    "name": "sensor.%air_quality%",
+    "device_class": "aqi",
+    "units_of_measurement": ["AQI"]
+}
+
